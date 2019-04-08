70d71f1eb4e49596be8e139730add4360ac5d5c4
Robin Bisping <robin@bisping.ch>
2019-04-08 14:14:39 +0200

    Fix language bug by setting default language to english

--- website/landing/views.py
+++ website/landing/views.py
@@ -6,15 +6,15 @@ def home(request, language=None):
     """
     Homepage
     """
-    # Let browser choose language if none is given
-    if language is None:
-        return home(request, request.META['HTTP_ACCEPT_LANGUAGE'])
-
     # Available languages
     languages = [
         ('de', 'Deutsch'),
         ('en', 'English'),
     ]
+
+    # Set language to english if none is given
+    if language is None:
+        language = 'en'
 
     # Render template if the language is defined
     if language in [lang[0] for lang in languages]:
